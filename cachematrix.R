28780fe2dc10377caffde3e16d9cfd58a9090937
Marcos Martins <36088852+marcmartinso@users.noreply.github.com>
2020-09-14 12:26:04 -0300

    cachematrix function - Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+###makeCacheMatrix function
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function (x = as.matrix()) {
+  m <- NULL
+  set <- function (y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x #retrieve the value of x
+  setmatrix <- function(inv.mat) m <<- inv.mat #set retrieve the value of matrix
+  getmatrix <- function() # retrieve the value of matrix
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix) # return list of makeCacheMatrix objects
+  
 }
+# the result is an object makeCacheMatrix which will be passed to the following
 
+# cacheSolve function
 
-## Write a short comment describing this function
+###Cachesolve function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!is.null(m)) { # test if the value of is null or not and return a message
+    message("getting cached data")  
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
+  m
 }
+
