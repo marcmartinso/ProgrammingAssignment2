6eb65e80f36ebdc8a661fd9194c9f91fa7e56227
Marcos Martins <36088852+marcmartinso@users.noreply.github.com>
2020-09-15 10:43:29 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,35 @@
 ###makeCacheMatrix function
 
-makeCacheMatrix <- function (x = as.matrix()) {
-  m <- NULL
+makeCacheMatrix <- function (x = matrix()) {
+  inverse <- NULL
   set <- function (y) {
     x <<- y
-    m <<- NULL
+    inverse <<- NULL
   }
   get <- function() x #retrieve the value of x
-  setmatrix <- function(inv.mat) m <<- inv.mat #set retrieve the value of matrix
-  getmatrix <- function() # retrieve the value of matrix
+  setInverse <- function(inv.mat) inverse <<- inv.mat #set retrieve the value of matrix
+  getInverse <- function() inverse # retrieve the value of matrix
   list(set = set, get = get,
-       setmatrix = setmatrix,
-       getmatrix = getmatrix) # return list of makeCacheMatrix objects
+       setInverse = setInverse,
+       getInverse = getInverse) # return list of makeCacheMatrix objects
   
 }
 # the result is an object makeCacheMatrix which will be passed to the following
 
-###cacheSolve function
+###Cachesolve function
 
 cacheSolve <- function(x, ...) {
-  m <- x$getmatrix()
-  if(!is.null(m)) { # test if the value of is null or not and return a message
+  inverse <- x$getInverse()
+  if(!is.null(inverse)) { # test if the value of is null or not and return a inverseessage
     message("getting cached data")  
-    return(m)
+    return(inverse)
   }
   data <- x$get()
-  m <- solve(data, ...)
-  x$setmatrix(m)
-  m
+  inverse <- solve(data, ...)
+  x$setInverse(inverse)
+  inverse
 }
+
+# cacheSolve uses the object created with makeCacheMatrix, uses its x$getInverse() possible 
+# with the Lexical Scoping and tests if it is null, If it is not null, return a message with 
+# its value, and if its is null it calculates the inverse of the matrix.
